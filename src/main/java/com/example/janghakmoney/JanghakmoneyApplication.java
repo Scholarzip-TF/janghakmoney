578bd405923fdbd7670b2f02a6bd64f9ea8af470
devkev00 <kimminsoo116@gmail.com>
2025-01-16 22:45:47 +0900

    Initial commit

---
+++ src/main/java/com/example/janghakmoney/JanghakmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.janghakmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanghakmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanghakmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/janghakmoney/JanghakmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.janghakmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanghakmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
