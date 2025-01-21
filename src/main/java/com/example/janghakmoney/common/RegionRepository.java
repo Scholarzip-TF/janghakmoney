b7b0451979fa031b560bb4cc0366194d1c38aba2
shwn <zxz8673@gmail.com>
2025-01-21 16:33:33 +0900

    .

--- src/main/java/com/example/janghakmoney/common/RegionRepository.java
+++ src/main/java/com/example/janghakmoney/common/RegionRepository.java
@@ -1,4 +1,9 @@
 package com.example.janghakmoney.common;
 
-public class RegionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region, String> {
+    // 기본적으로 JpaRepository 메서드 사용 가능 (예: findById)
 }
