ffb869a32160d1fafc1fc195fc73e2695e2ffec0
shwn <zxz8673@gmail.com>
2025-02-08 18:52:52 +0900

    fix: CORS 해결 시도

--- src/main/java/com/example/janghakmoney/SecurityConfig.java
+++ src/main/java/com/example/janghakmoney/SecurityConfig.java
@@ -3,8 +3,14 @@ package com.example.janghakmoney;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -12,12 +18,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // X-Frame-Options 설정: iframe 허용
-                .headers(headers -> headers
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .headers(headers -> headers // X-Frame-Options 설정: iframe 허용
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)  // 동일 도메인 허용
 //                        .frameOptions(frameOptions -> frameOptions.allowFrom("https://example.com"))  // 특정 도메인 허용
                 )
-                .csrf(csrf -> csrf.disable());  // CSRF 보호 비활성화
+                .csrf(AbstractHttpConfigurer::disable);  // CSRF 보호 비활성화
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://scholarzip.kr", "https://scholarzip.kr", "http://www.scholarzip.kr", "https://www.scholarzip.kr", "https://findscholarzip.netlify.app"));  // 필요한 도메인만 명시할 수 있음
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
